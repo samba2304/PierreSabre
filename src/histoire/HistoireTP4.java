c5e1bd15afa2a98fa91538095b5ae0dcec836d27
samba2304 <samba.ndiaye2304@gmail.com>
2024-04-09 09:39:18 +0200

    Faire Connaisance

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -10,28 +10,29 @@ import personnages.Yakuza;
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-		Humain prof = new Humain("Prof", "kombucha", 54);
-		//prof.parler(prof.direBonjour());
-		//prof.acheter("boisson", 12);
-		//prof.boire();
-		//prof.acheter("jeu", 2);
-		//prof.acheter("kimono", 50);
-
-		Ronin roro = new Ronin("Roro", "shochu",54);
 		Commercant marco = new Commercant("Marco", 20);
-		// marco.parler(marco.direBonjour());
-		// marco.seFaireExtorquer();
-		// marco.recevoir(15);
-		// marco.boire();
+		Commercant chonin = new Commercant("Chonin", 40);
+		Commercant kumi = new Commercant("Kumi", 10);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+		marco.direBonjour();
+		roro.direBonjour();
+		marco.direBonjour();
+		yaku.direBonjour();
+		marco.direBonjour();
+		chonin.direBonjour();
+		marco.direBonjour();
+		kumi.direBonjour();
+		marco.direBonjour();
+		marco.faireConnaissanceAvec(roro);
+		marco.faireConnaissanceAvec(yaku);
+		marco.faireConnaissanceAvec(chonin);
+		marco.faireConnaissanceAvec(kumi);
+		marco.listerConnaissance();
+		roro.listerConnaissance();
+		yaku.listerConnaissance();
 
-		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 45, "Warsong");
-		//yaku.parler(yaku.direBonjour());
-		//yaku.extorquer(marco);
-		
-		//roro.parler(roro.direBonjour());
-		//roro.donner(marco);
-		
-		roro.provoquer(yaku);
+	
 	}
 
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,11 +4,66 @@ public class Humain {
 	protected String nom;
 	protected String boissonPref;
 	protected int argent;
+	private static final int TAILLE_TAB = 30;
+	private int nbHumain = 0;
+	protected Humain[] memoire = new Humain[TAILLE_TAB];
+	protected int plusancien = 0; // Humain plus ancien stockéé
+	protected int plusrecent = 0; // Humain plus recent stockéé
 
 	public Humain(String nom, String boissonPref, int argent) {
 		this.nom = nom;
 		this.boissonPref = boissonPref;
 		this.argent = argent;
+	}
+
+	public void faireConnaissanceAvec(Humain homme2) {
+		parler(direBonjour());
+		homme2.repondre(this);
+		memoriser(homme2);
+	}
+
+	public void repondre(Humain homme1) {
+		parler(direBonjour());
+		memoriser(homme1);
+	}
+
+	public void memoriser(Humain humain) {
+		int tableaumodifie = 0; // tableau pas modifie
+		if (plusrecent == TAILLE_TAB) {
+			plusrecent = 0;
+		}
+		if (nbHumain == TAILLE_TAB) {
+			nbHumain = TAILLE_TAB;
+		}
+		if (memoire[plusrecent] == null) {
+			memoire[plusrecent] = humain;
+			plusrecent++;
+			nbHumain++;
+			tableaumodifie = 1;
+		}
+
+		if (tableaumodifie == 0) {
+			if (plusancien == TAILLE_TAB) {
+				plusancien = 0;
+			}
+			memoire[plusancien] = humain;
+			plusancien++;
+		}
+	}
+
+	public void listerConnaissance() {
+		String reponse = "";
+		System.out.println(nbHumain);
+		for (int i = 0; i < nbHumain; i++) {
+			if (i == nbHumain-1) {
+				reponse += memoire[i].getNom();
+
+			} else {
+				reponse += memoire[i].getNom() + ",";
+			}
+		}
+		parler("Je connais beaucoup de monde dont :" + reponse);
+
 	}
 
 	public void parler(String phrase) {
